e9750b2eedee741de0cee70c2977e847b8d46ea8
Anshu <riyanshu062@gmail.com>
2025-11-21 18:48:41 +0530

    Create sync & async file

---
+++ SEASON_01/async.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const https = require("https");
+
+console.log("Hello World");
+
+var a = 100;
+var b = 100;
+
+// API Call
+https.get("https://dummyjson.com/products/1", (res) => {
+    console.log("Fetched Data Successfully");
+});
+
+// To execute the code after milisecond
+setTimeout(() => {
+    console.log("setTimeout called after 5 seconds");
+}, 5000);
+
+// To read the code of the path of file.txt
+fs.readFile("./file.txt", "utf8", (err, data) => {
+    console.log("File Data :", data);
+});
+
+
+function multiplyFn(x, y) {
+    const result = a * b;
+    return result;
+}
+
+var c = multiplyFn(a, b);
+
+console.log("Multiplication result is :", c);
---
+++ SEASON_01/sync.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+var a = 50;
+var b = 20;
+
+function multiplyFn(x, y){
+    const result = a * b;
+    return result;
+}
+var c = multiplyFn(a, b);
+console.log("Multiplication result is : ",c);
