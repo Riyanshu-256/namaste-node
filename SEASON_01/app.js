34fc95ba4c3f72f67a33eab293d9935f4ab1fe12
Anshu <riyanshu062@gmail.com>
2025-11-21 18:14:48 +0530

    Update

---
+++ SEASON_01/app.js
@@ -0,0 +1,15 @@
+require("../xyz.js");
+
+const { calculateSum, calculateMultiply, x } = require("../Calculate/index.js");
+
+const data = require("../data.json");
+
+console.log(data);
+
+var name = "Riaynshu Sharma";
+
+var a = 10;
+var b = 10;
+
+calculateMultiply(a, b);
+calculateSum(a, b);
