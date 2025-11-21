1fde3431c865694509105004aab52bb015faee67
Anshu <riyanshu062@gmail.com>
2025-11-21 16:28:02 +0530

    require & experts.modules

--- xyz.js
+++ xyz.js
@@ -1,1 +1,15 @@
-console.log("Very important code");
+ 
+// rrequire("./path")
+// All the code of the module wrapped inside a function(IIFE)
+
+// Immediately Invoked Function Expressiona => IIFE
+
+(function () {
+    // All code of the module runs inside the function
+})();  
+
+// To check path of the file
+console.log(__filename);
+
+// To check code or repo directory
+console.log(__dirname);
