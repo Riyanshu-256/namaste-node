853931308264a42ddb973a8e451e22a15f8f58a2
Anshu <riyanshu062@gmail.com>
2025-11-22 12:17:56 +0530

    Event Loop Example

---
+++ SEASON_01/EVENT_LOOP/event-loop-2.js
@@ -0,0 +1,20 @@
+const a = 100;
+
+setImmediate(() => console.log("setImmediate"));
+
+Promise.resolve(() => console.log("Promise"));
+
+fs.readFile("./file.txt", "utf8", () => {
+  console.log("File Reading CB");
+});
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+process.nextTick(() => console.log("process.nextTick"));
+
+function printA() {
+  console.log("a =", a);
+}
+
+printA();
+console.log("Last line of the file.");
---
+++ SEASON_01/EVENT_LOOP/event-loop-4.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+setImmediate(() => console.log("setImmediate"));
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+Promise.resolve(() => console.log("Promise"));
+
+fs.readFile("./file.txt", "utf8", () => {
+  console.log("File Reading CB");
+
+  setTimeout(() => console.log("2nd timer"), 0);
+
+  setImmediate(() => console.log("2nd setImmediate"));
+});
+
+process.nextTick(() => {
+  process.nextTick(() => console.log("inner nextTick"));
+  console.log("nextTick");
+});
+
