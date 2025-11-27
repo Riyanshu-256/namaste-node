e16f5e0c4100e6fa34446b83df94871cf4894cfd
Anshu <riyanshu062@gmail.com>
2025-11-27 07:36:46 +0530

    Middleware

--- SEASON_2(Project)/src/app.js
+++ SEASON_2(Project)/src/app.js
@@ -4,6 +4,12 @@ const express = require("express");
 // to create application
 const app = express();
 
+// Middleware
+app.use("/", (req, res, next) => {
+    res.send("Handling the middleware!!!");
+    next();
+});
+
 app.get(
     "/user",
     (req, res, next) => {
@@ -12,23 +18,8 @@ app.get(
     },
     (req, res, next) => {
         console.log("Handling the route user 2!!!");
-        // res.send("2nd Response!!");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 3!!!");
-        // res.send("3rd Response!!");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 4!!!");
-        // res.send("4th Response!!");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 5!!!");
         res.send("2nd Response!!");
-    },
+    }
 );
 
 //-------------------------------------------------------------------------------------------------------------------------------------//
