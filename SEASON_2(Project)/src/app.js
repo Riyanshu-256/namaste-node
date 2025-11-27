36d5dfe11b4d64e43f78e5727c57b4ef33964f6f
Anshu <riyanshu062@gmail.com>
2025-11-27 06:06:19 +0530

    Routing

--- SEASON_2(Project)/src/app.js
+++ SEASON_2(Project)/src/app.js
@@ -4,14 +4,35 @@ const express = require("express");
 // to create application
 const app = express();
 
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({
+        firstName: "Riyanshu",
+        lastName: "Sharma",
+        city: "Koderma",
+        age: 22
+    })
+});
+
+app.post("/user", (req, res) => {
+    // Save data to the database
+    res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+});
+
+// THIS WILL MATCH ALL THE HTTP METHOD API CALLS /test
 // to handle request for "/" , "/test", "/hello"
+app.use("/", (req, res) => {
+    res.send("Hello from the empty soul");
+});
+
 app.use("/test", (req, res) => {
     res.send("Hello from the server");
 });
 
-app.use("/", (req, res) => {
-    res.send("Hello from the empty soul");
-});
 
 app.use("/hello", (req, res) => {
     res.send("Hello what are you doing???");
