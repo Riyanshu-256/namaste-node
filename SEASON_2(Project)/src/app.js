00f59193f6c806a799f957c7595b8736e5cd9388
Anshu <riyanshu062@gmail.com>
2025-11-28 18:28:27 +0530

    modal types

--- SEASON_2(Project)/src/app.js
+++ SEASON_2(Project)/src/app.js
@@ -10,23 +10,87 @@ const User = require("./models/user");
 // to create application
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
 
     // Creating a new instance of the User model
-    const user = new User({
-        firstName: "Aditya",
-        lastName: "Sharma",
-        emailId: "aditya@gmail.com",
-        password: "aditya@1234"
-    });
+    const user = new User(req.body);
     try {
         await user.save();
     res.send("User added successfully!!!");
     } catch (err){
-        res.send(401).send("Error saving the user:" + err.message);
+        res.status (401).send("Error saving the user:" + err.message);
     }
-})
+});
 
+// How to find 1 user from the database
+app.get("/user", async (req, res) => {
+
+    // Reading emailId from the request body
+    const userEmail = req.body.emailId;
+
+    try {
+        // Find one users matching the given emailId
+        const users = await User.findOne({ emailId: userEmail });
+        if (!users){ 
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+        // // Find all users matching the given emailId
+        // const users = await User.find({ emailId: userEmail });
+
+        // // If no user found, send 404 error
+        // if (users.length === 0) {
+        //     res.status(404).send("User not found");
+        // } else {
+        //     // If user found, send user data
+        //     res.send(users);
+        // }
+
+    } catch (err) {
+        // If any error occurs in try block, send error message
+        res.status(401).send("Something went wrong");
+    }
+});
+
+// Feed API - GET/feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        // If any error occurs in try block, send error message
+        res.status(401).send("Something went wrong");
+    }
+
+});
+
+// Delete a user from the database
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch (err){
+        res.status(401).send("Something went wrong");
+    }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({_id: userId}, data);
+        res.send("User updated successfully");
+    } catch (err){
+        res.status(401).send("Something went wrong");
+    }
+});
 
 // Firstly call the connectDB(), try to connect to the database
 connectDB()
