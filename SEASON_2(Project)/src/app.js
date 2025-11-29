57e5997cc82a4d9ef96c384a4a1f6202b351a549
Anshu <riyanshu062@gmail.com>
2025-11-29 07:48:39 +0530

    api level data sanization

--- SEASON_2(Project)/src/app.js
+++ SEASON_2(Project)/src/app.js
@@ -81,10 +81,23 @@ app.delete("/user", async (req, res) => {
 });
 
 // Update data of the user
-app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
     const data = req.body;
+
     try {
+        const ALLOWED_UPDATES = ["about", "gender", "age", "skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k) => 
+            ALLOWED_UPDATES.includes(k)
+        );
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length > 10) {
+            throw new Error("Skills can't be more than 10")
+        }
+
         const user = await User.findByIdAndUpdate({_id: userId}, data, {
             returnDocument: "after",
             runValidators: true,  // create a custom validation schema
