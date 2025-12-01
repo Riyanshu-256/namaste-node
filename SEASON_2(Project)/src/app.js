8eebd5be59f6b21138fb4c8d7b8adade89da4ae8
Anshu <riyanshu062@gmail.com>
2025-12-01 07:57:06 +0530

    Hashing

--- SEASON_2(Project)/src/app.js
+++ SEASON_2(Project)/src/app.js
@@ -134,7 +134,7 @@ app.patch("/user/:userId", async (req, res) => {
     const data = req.body;
 
     try {
-        const ALLOWED_UPDATES = ["about", "gender", "age", "skills"];
+        const ALLOWED_UPDATES = ["about", "gender", "age", "skills", "password"];
 
         const isUpdateAllowed = Object.keys(data).every((k) => 
             ALLOWED_UPDATES.includes(k)
